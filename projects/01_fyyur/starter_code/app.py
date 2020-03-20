3376b31f87a801afbbbd28e454fd0f42df468456
Anastasia Lisina <stacy@portland.redmond.dp-net.com>
2020-03-19 21:24:36 -0700

    changed Show model to include additional data

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -29,13 +29,13 @@ migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
-
+"""
 shows = db.Table('Shows',
                 db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key = True),
                 db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key = True),
                 db.Column('start_time', db.DateTime, nullable = False)
 )
-
+"""
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
@@ -52,8 +52,8 @@ class Venue(db.Model):
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String)
-    artists = db.relationship('Artist', secondary = shows, 
-             backref = db.backref('venues', lazy = True))
+    shows = db.relationship('Show', back_populates='venues')
+
 
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
@@ -72,7 +72,17 @@ class Artist(db.Model):
     website = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String())
+    shows = db.relationship('Show', back_populates='artists')
 
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key = True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), primary_key = True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), primary_key = True)
+    start_time = db.Column(db.DateTime, nullable = False)
+    venues = db.relationship(Venue, back_populates='shows')
+    artists = db.relationship(Artist, back_populates='shows')
 
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
@@ -400,7 +410,6 @@ def show_artist(artist_id):
   data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
   """
   data = Artist.query.get(artist_id)
-  print(data.genres)
   return render_template('pages/show_artist.html', artist=data)
 
 #  Update
@@ -485,6 +494,7 @@ def create_artist_submission():
     flash('Artist ' + request.form['name'] + ' was successfully listed!')
   except:
     db.session.rollback()
+    print(sys.exc_info())
     flash('An error occurred. Artist ' + request.form['name']+ ' could not be listed.')
   finally:
     db.session.close()
@@ -550,9 +560,20 @@ def create_shows():
 def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
-
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
+  try:
+    show = Show()
+    show.venue_id = request.form['venue_id']
+    show.artist_id = request.form['artist_id']
+    show.start_time = request.form['start_time']   
+    # on successful db insert, flash success
+    flash('Show was successfully listed!') 
+  except:
+    db.session.rollback()
+    print(sys.exc_info())
+    flash('An error occurred. Show could not be listed.')
+  finally:
+    db.session.close()
+  
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Show could not be listed.')
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
